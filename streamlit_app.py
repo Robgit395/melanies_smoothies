30d945ae8f63bfa029bb2ac424162741b38f7755
Robgit395 <rob.andrews@merckgroup.com>
2025-09-03 10:17:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ st.stop()
 
 # copy for pandas loc
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
